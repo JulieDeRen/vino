d2d513edaac147197929432373f1a6e90bdbd450
rompo80 <romanworld@gmail.com>
2023-04-15 09:47:39 -0400

    Modifié le fichier App.js pour inclure la navigation et footer sur toutes les pages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,18 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
+import Vue from 'vue';
+import router from './routes';
+import '../css/editApp.css'
+import Navigation from './components/Navigation.vue';
+import Footer from './components/Footer.vue';
 
-window.Vue = require('vue').default;
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.component('navigation', Navigation);
+Vue.component('footer-component', Footer);
 
 const app = new Vue({
-    el: '#app',
-});
+    router,
+    render: h => h('div', { class: 'flex flex-col min-h-screen' }, [
+        h('navigation'),
+        h('main', { class: 'flex-1' }, [ h('router-view') ]),
+        h('footer-component')
+      ])
+}).$mount('#app');
