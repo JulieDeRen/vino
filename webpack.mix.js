7f953b434490d41d87597ee32842bc1f7a0eccc9
rompo80 <romanworld@gmail.com>
2023-04-16 02:08:57 -0400

    installe browser-sync-webpack-plugin et modifie le fichier webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require('laravel-mix');
-
+const tailwindcss = require('tailwindcss');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -14,5 +15,34 @@ mix.js("resources/js/app.js", "public/js")
  .vue()
  .postCss("resources/css/app.css", "public/css", [
    require("tailwindcss"),
- ]);
+ ])
+ .webpackConfig({
+  plugins: [
+      new BrowserSyncPlugin({
+          proxy: 'http://localhost:8000',
+          browser: 'google chrome',
+          port: 3000,
+          files: ['public/**/*.{js,css}']
+      })
+  ]
+});
 mix.disableNotifications()
+
+
+
+
+// const mix = require('laravel-mix');
+// const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+// mix.js('resources/js/app.js', 'public/js')
+//    .sass('resources/sass/app.scss', 'public/css')
+//    .webpackConfig({
+//        plugins: [
+//            new BrowserSyncPlugin({
+//                proxy: 'http://localhost:8000',
+//                browser: 'google chrome',
+//                port: 3000,
+//                files: ['public/**/*.{js,css}']
+//            })
+//        ]
+//    });
