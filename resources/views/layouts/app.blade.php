92d49408e7ea76b9fd77f3283a75f0ec726fb459
JulieDeRen <julie.deschenes-renaud@hec.ca>
2023-04-18 16:07:49 -0400

    Ajout d'un formulaire pour ajout de bouteilles sur le cellier

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,10 +27,10 @@
 <body>
   <div id="app">
     @include('layouts.nav')
-</div>
     <main class="py-4">
       @yield('content')
     </main>
     @include('layouts.footer')
+  </div>
 </body>
 </html>
