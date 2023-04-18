b76f2563576f162c7493a83c753e3fa2d9f14b17
rompo80 <romanworld@gmail.com>
2023-04-18 10:48:05 -0400

    Ajouter ficheCarte dans le dossier template

---
+++ templates/ancienHomePage.php
@@ -0,0 +1,137 @@
+<!-- <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+<div class="grid gap-5 lg:grid-cols-4 sm:max-w-sm sm:mx-auto  lg:max-w-full">
+<div class="overflow-hidden transition-shadow duration-300 max-w-full mx-auto bg-white rounded  max-h-[550px]">
+  <a href="/" aria-label="Article"><img src="https://www.saq.com/media/catalog/product/1/5/15116225-1_1679602083.png?width=367&amp;height=550&amp;canvas=367,550&amp;quality=80&amp;fit=bounds" class="object-cover max-h-[350px] rounded" alt="" /></a>
+  <div class="py-5">
+    <p class="mb-2 text-xs font-semibold text-gray-600 uppercase">
+      13 Jul 2020
+    </p>
+    <span class="mb-2 font-semibold">Cellier</span>
+    <a href="/" aria-label="Article" class="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"><p class="sm:text-2xl text-xl font-bold leading-6">André Rohrer Stein Riesling</p></a>
+    <div class="flex space-x-4">
+      <a href="/" aria-label="Comments" class="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group">
+        <div class="mr-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            stroke="currentColor"
+            class="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+          >
+            <polyline points="23 5 23 18 19 18 19 22 13 18 12 18" fill="none" stroke-miterlimit="10"></polyline>
+            <polygon points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2" fill="none" stroke="currentColor" stroke-miterlimit="10"></polygon>
+          </svg>
+        </div>
+        <p class="font-semibold">81</p>
+      </a>
+    </div>
+  </div>
+</div> -->
+
+<!-- <div class="overflow-hidden transition-shadow duration-300 bg-white rounded">
+        <a href="/" aria-label="Article"><img src="https://www.saq.com/media/catalog/product/1/4/14205357-1_1604349331.png?width=367&amp;height=550&amp;canvas=367,550&amp;quality=80&amp;fit=bounds" class="object-cover w-full h-70 rounded" alt="" /></a>
+        <div class="py-5">
+          <p class="mb-2 text-xs font-semibold text-gray-600 uppercase">
+            4 Nov 2020
+          </p>
+          <span class="mb-2 font-semibold">Cellier</span>
+          <a href="/" aria-label="Article" class="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"><p class="text-2xl font-bold leading-5">Alain Jaume Côtes du Rhône Grand Veneur 2021</p></a>
+          <div class="flex space-x-4">
+            <a href="/" aria-label="Comments" class="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group">
+              <div class="mr-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  class="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                >
+                  <polyline points="23 5 23 18 19 18 19 22 13 18 12 18" fill="none" stroke-miterlimit="10"></polyline>
+                  <polygon points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2" fill="none" stroke="currentColor" stroke-miterlimit="10"></polygon>
+                </svg>
+              </div>
+              <p class="font-semibold">81</p>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="overflow-hidden transition-shadow duration-300 bg-white rounded">
+        <a href="/" aria-label="Article"><img src="https://www.saq.com/media/catalog/product/1/4/14123845-1_1578552027.png?width=367&amp;height=550&amp;canvas=367,550&amp;quality=80&amp;fit=bounds" class="object-cover w-full h-70 rounded" alt="" /></a>
+        <div class="py-5">
+          <p class="mb-2 text-xs font-semibold text-gray-600 uppercase">
+            28 Dec 2020
+          </p>
+          <span class="mb-2 font-semibold">Cellier</span>
+          <a href="/" aria-label="Article" class="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700">
+            <p class="text-2xl font-bold leading-5">
+              Albert Bichot Domaine du Pavillon Aloxe-Corton 2020
+            </p>
+          </a>
+          <div class="flex space-x-4">
+            <a href="/" aria-label="Comments" class="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group">
+              <div class="mr-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  class="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                >
+                  <polyline points="23 5 23 18 19 18 19 22 13 18 12 18" fill="none" stroke-miterlimit="10"></polyline>
+                  <polygon points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2" fill="none" stroke="currentColor" stroke-miterlimit="10"></polygon>
+                </svg>
+              </div>
+              <p class="font-semibold">81</p>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="overflow-hidden transition-shadow duration-300 bg-white rounded">
+        <a href="/" aria-label="Article"><img src="https://www.saq.com/media/catalog/product/1/4/14123845-1_1578552027.png?width=367&amp;height=550&amp;canvas=367,550&amp;quality=80&amp;fit=bounds" class="object-cover w-full h-70 rounded" alt="" /></a>
+        <div class="py-5">
+          <p class="mb-2 text-xs font-semibold text-gray-600 uppercase">
+            28 Dec 2020
+          </p>
+          <span class="mb-2 font-semibold">From cellar:</span>
+          <a href="/" aria-label="Article" class="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700">
+            <p class="text-2xl font-bold leading-5">
+              Albert Bichot Domaine du Pavillon Aloxe-Corton 2020
+            </p>
+          </a>
+          <div class="flex space-x-4">
+            <a href="/" aria-label="Comments" class="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group">
+              <div class="mr-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  class="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                >
+                  <polyline points="23 5 23 18 19 18 19 22 13 18 12 18" fill="none" stroke-miterlimit="10"></polyline>
+                  <polygon points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2" fill="none" stroke="currentColor" stroke-miterlimit="10"></polygon>
+                </svg>
+              </div>
+              <p class="font-semibold">81</p>
+            </a>
+          </div>
+        </div>
+      </div> -->
+<!-- </div>
+  </div> -->
