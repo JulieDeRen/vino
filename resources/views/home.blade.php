d6e372cf2795797ccb9e417aee2f8c653bae6c70
Lbarreiros1980 <88776130+Lbarreiros1980@users.noreply.github.com>
2023-04-18 15:46:50 -0400

    ajout du catalogue avec bouteilles - in progress!

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,51 +78,182 @@ btn.onclick = function() {
   modal.style.display = "flex";
 };
 </script> --}}
-  <div class="p-4 flex flex-col items-center sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 space-y-1.5">
-  <div class="w-80 h-full bg-white shadow rounded ">
-    <div class="grid gap-5 lg:grid-cols-4 sm:max-w-sm sm:mx-auto  lg:max-w-full">
-      <div class="overflow-hidden transition-shadow duration-300 max-w-full mx-auto bg-white rounded  max-h-[550px]">
-          <a href="/" aria-label="Article"><img src="https://www.saq.com/media/catalog/product/1/5/15116225-1_1679602083.png?width=367&amp;height=550&amp;canvas=367,550&amp;quality=80&amp;fit=bounds" class="object-cover max-h-[350px] rounded" alt="" /></a>
-          <div class="py-5">
-          <p class="mb-2 text-xs font-semibold text-gray-600 uppercase">
-              13 Jul 2020
-          </p>
-          <span class="mb-2 font-semibold">Cellier</span>
-          <a href="/" aria-label="Article" class="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700">
-              <p class="sm:text-2xl text-xl font-bold leading-6">André Rohrer Stein Riesling</p>
-          </a>
-        <div class="flex space-x-4">
-              <a href="/" aria-label="Comments" class="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group">
-                  <div class="mr-2">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" class="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700">
-                          <polyline points="23 5 23 18 19 18 19 22 13 18 12 18" fill="none" stroke-miterlimit="10"></polyline>
-                          <polygon points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2" fill="none" stroke="currentColor" stroke-miterlimit="10"></polygon>
-                      </svg>
-                  </div>
-                  <p class="font-semibold">81</p>
-                </a>
+<section style="background-color: #eee;">
+  <div class="container py-5 px-5">
+    <div class="row justify-content-center mb-3">
+      <div class="col-md-12 col-xl-10">
+        <div class="card shadow-0 border rounded-3">
+          <div class="card-body">
+            <div class="row">
+              {{-- START Carte Bouteille Catalogue --}}
+              <div class="p-4 flex flex-col items-center sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 space-y-1.5">
+              <div class="w-80 h-full bg-white shadow rounded ">
+                <div class="px-5 py-5 grid gap-5 lg:grid-cols-4 sm:max-w-sm sm:mx-auto  lg:max-w-full">
+                  <div class="overflow-hidden transition-shadow duration-300 max-w-full mx-auto bg-white rounded  max-h-[550px]">
+                      <a href="/" aria-label="Article"><img src="https://www.saq.com/media/catalog/product/1/5/15116225-1_1679602083.png?width=367&amp;height=550&amp;canvas=367,550&amp;quality=80&amp;fit=bounds" class="object-cover max-h-[350px] rounded" alt="" /></a>
+                      <div class="py-5">
+                      <p class="mb-2 text-xs font-semibold text-gray-600 uppercase">
+                          13 Jul 2020
+                      </p>
+                      <span class="mb-2 font-semibold">Cellier</span>
+                      <a href="/" aria-label="Article" class="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700">
+                          <p class="sm:text-2xl text-xl font-bold leading-6">André Rohrer Stein Riesling</p>
+                      </a>
+                    <div class="flex space-x-4">
+                          <a href="/" aria-label="Comments" class="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group">
+                              <div class="mr-2">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" class="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700">
+                                      <polyline points="23 5 23 18 19 18 19 22 13 18 12 18" fill="none" stroke-miterlimit="10"></polyline>
+                                      <polygon points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2" fill="none" stroke="currentColor" stroke-miterlimit="10"></polygon>
+                                  </svg>
+                              </div>
+                              <p class="font-semibold">81</p>
+                            </a>
+                      </div>
+                    <div class="p-4 flex flex-col items-center">
+                      <div class="inline-flex items-center mt-3">
+                        <button class="bg-white rounded-l border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200"> 
+                          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 24 24"  stroke="currentColor"> 
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4" />
+                          </svg>
+                        </button> 
+                      <div class="bg-gray-100 border-t border-b border-gray-100 text-gray-600 hover:bg-gray-100 inline-flex items-center px-4 py-1 select-none"> 2 
+                      </div>
+                          <button class="bg-white rounded-r border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200"> 
+                          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 24 24"  stroke="currentColor" > 
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
+                          </svg> 
+                          </button>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            </div>
+            {{--  END Carte Bouteille Catalogue --}}
           </div>
-        <div class="p-4 flex flex-col items-center">
-          <div class="inline-flex items-center mt-3">
-            <button class="bg-white rounded-l border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200"> 
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 24 24"  stroke="currentColor"> 
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4" />
-              </svg>
-            </button> 
-          <div class="bg-gray-100 border-t border-b border-gray-100 text-gray-600 hover:bg-gray-100 inline-flex items-center px-4 py-1 select-none"> 2 
-          </div>
-              <button class="bg-white rounded-r border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200"> 
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 24 24"  stroke="currentColor" > 
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
-              </svg> 
-              </button>
         </div>
         </div>
-        </div>
+      </div>
     </div>
-  </div>
-</div>
-</div>
+    
+    <div class="row justify-content-center mb-3">
+      <div class="col-md-12 col-xl-10">
+        <div class="card shadow-0 border rounded-3">
+          <div class="card-body">
+            <div class="row">
+              {{-- START Carte Bouteille Catalogue --}}
+              <div class="p-4 flex flex-col items-center sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 space-y-1.5">
+              <div class="w-80 h-full bg-white shadow rounded ">
+                <div class="px-5 py-5 grid gap-5 lg:grid-cols-4 sm:max-w-sm sm:mx-auto  lg:max-w-full">
+                  <div class="overflow-hidden transition-shadow duration-300 max-w-full mx-auto bg-white rounded  max-h-[550px]">
+                      <a href="/" aria-label="Article"><img src="https://www.saq.com/media/catalog/product/1/5/15116225-1_1679602083.png?width=367&amp;height=550&amp;canvas=367,550&amp;quality=80&amp;fit=bounds" class="object-cover max-h-[350px] rounded" alt="" /></a>
+                      <div class="py-5">
+                      <p class="mb-2 text-xs font-semibold text-gray-600 uppercase">
+                          13 Jul 2020
+                      </p>
+                      <span class="mb-2 font-semibold">Cellier</span>
+                      <a href="/" aria-label="Article" class="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700">
+                          <p class="sm:text-2xl text-xl font-bold leading-6">André Rohrer Stein Riesling</p>
+                      </a>
+                    <div class="flex space-x-4">
+                          <a href="/" aria-label="Comments" class="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group">
+                              <div class="mr-2">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" class="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700">
+                                      <polyline points="23 5 23 18 19 18 19 22 13 18 12 18" fill="none" stroke-miterlimit="10"></polyline>
+                                      <polygon points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2" fill="none" stroke="currentColor" stroke-miterlimit="10"></polygon>
+                                  </svg>
+                              </div>
+                              <p class="font-semibold">81</p>
+                            </a>
+                      </div>
+                    <div class="p-4 flex flex-col items-center">
+                      <div class="inline-flex items-center mt-3">
+                        <button class="bg-white rounded-l border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200"> 
+                          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 24 24"  stroke="currentColor"> 
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4" />
+                          </svg>
+                        </button> 
+                      <div class="bg-gray-100 border-t border-b border-gray-100 text-gray-600 hover:bg-gray-100 inline-flex items-center px-4 py-1 select-none"> 2 
+                      </div>
+                          <button class="bg-white rounded-r border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200"> 
+                          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 24 24"  stroke="currentColor" > 
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
+                          </svg> 
+                          </button>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            </div>
+            {{--  END Carte Bouteille Catalogue --}}
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center mb-3">
+      <div class="col-md-12 col-xl-10">
+        <div class="card shadow-0 border rounded-3">
+          <div class="card-body">
+            <div class="row">
+              {{-- START Carte Bouteille Catalogue --}}
+              <div class="p-4 flex flex-col items-center sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 space-y-1.5">
+              <div class="w-80 h-full bg-white shadow rounded ">
+                <div class="px-5 py-5 grid gap-5 lg:grid-cols-4 sm:max-w-sm sm:mx-auto  lg:max-w-full">
+                  <div class="overflow-hidden transition-shadow duration-300 max-w-full mx-auto bg-white rounded  max-h-[550px]">
+                      <a href="/" aria-label="Article"><img src="https://www.saq.com/media/catalog/product/1/5/15116225-1_1679602083.png?width=367&amp;height=550&amp;canvas=367,550&amp;quality=80&amp;fit=bounds" class="object-cover max-h-[350px] rounded" alt="" /></a>
+                      <div class="py-5">
+                      <p class="mb-2 text-xs font-semibold text-gray-600 uppercase">
+                          13 Jul 2020
+                      </p>
+                      <span class="mb-2 font-semibold">Cellier</span>
+                      <a href="/" aria-label="Article" class="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700">
+                          <p class="sm:text-2xl text-xl font-bold leading-6">André Rohrer Stein Riesling</p>
+                      </a>
+                    <div class="flex space-x-4">
+                          <a href="/" aria-label="Comments" class="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group">
+                              <div class="mr-2">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" class="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700">
+                                      <polyline points="23 5 23 18 19 18 19 22 13 18 12 18" fill="none" stroke-miterlimit="10"></polyline>
+                                      <polygon points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2" fill="none" stroke="currentColor" stroke-miterlimit="10"></polygon>
+                                  </svg>
+                              </div>
+                              <p class="font-semibold">81</p>
+                            </a>
+                      </div>
+                    <div class="p-4 flex flex-col items-center">
+                      <div class="inline-flex items-center mt-3">
+                        <button class="bg-white rounded-l border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200"> 
+                          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 24 24"  stroke="currentColor"> 
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4" />
+                          </svg>
+                        </button> 
+                      <div class="bg-gray-100 border-t border-b border-gray-100 text-gray-600 hover:bg-gray-100 inline-flex items-center px-4 py-1 select-none"> 2 
+                      </div>
+                          <button class="bg-white rounded-r border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200"> 
+                          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 24 24"  stroke="currentColor" > 
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
+                          </svg> 
+                          </button>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            </div>
+            {{--  END Carte Bouteille Catalogue --}}
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    
+</section>
 @endsection
           
 
