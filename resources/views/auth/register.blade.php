f283c4f9035d67e98205775bf6c2b0288ef76feb
rompo80 <romanworld@gmail.com>
2023-04-20 15:35:55 -0400

    Change le navbar et menu.js, register form et login form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,42 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mx-auto">
-  <div class="flex justify-center">
-    <div class="w-full max-w-md">
-      <div class="bg-white rounded-lg overflow-hidden">
-        <div class="px-6 py-4">
-          <h2 class="text-center text-accent_wine text-2xl mb-4">{{ __('Inscription') }}</h2>
-          <form method="POST" action="{{ route('register') }}">
-            @csrf
-            <div class="mb-3">
-              <input id="name" type="text" placeholder="{{ __('Nom') }}" class="shadow appearance-none border rounded w-full py-3 px-3 text-text_primary leading-tight focus:outline-none focus:shadow-outline border-primary @error('name') border-red-500 @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-              @error('name')
-              <p class="text-red-500 text-xs italic mt-2">{{ $message }}</p>
-              @enderror
-            </div>
-            <div class="mb-3">
-              <input id="courriel" type="email" placeholder="{{ __('Email Address') }}" class="shadow appearance-none border rounded w-full py-3 px-3 text-text_primary leading-tight focus:outline-none focus:shadow-outline border-primary @error('courriel') border-red-500 @enderror" name="courriel" value="{{ old('courriel') }}" required autocomplete="courriel">
-              @error('courriel')
-              <p class="text-red-500 text-xs italic mt-2">{{ $message }}</p>
-              @enderror
-            </div>
-            <div class="mb-3">
-              <input id="password" type="password" placeholder="{{ __('Password') }}" class="shadow appearance-none border rounded w-full py-3 px-3 text-text_primary leading-tight focus:outline-none focus:shadow-outline border-primary @error('password') border-red-500 @enderror" name="password" required autocomplete="new-password">
-              @error('password')
-              <p class="text-red-500 text-xs italic mt-2">{{ $message }}</p>
-              @enderror
-            </div>
-            <div class="mb-4">
-              <input id="password-confirm" type="password" placeholder="{{ __('Confirm Password') }}" class="shadow appearance-none border rounded w-full py-3 px-3 text-text_primary leading-tight focus:outline-none focus:shadow-outline border-primary" name="password_confirmation" required autocomplete="new-password">
-            </div>
-            <div class="text-center">
-              <button type="submit" class="border border-primary text-accent_wine hover:bg-primary hover:text-main py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                {{ __('Inscription') }}
-              </button>
-            </div>
-          </form>
-        </div>
+
+<div class="container mb-3 mx-auto">
+  <div class="min-h-[500px] container flex justify-center items-center">
+    <div class="bg-white rounded-lg overflow-hidden max-w-[400px] w-full">
+      <div class="px-6 mb-5">
+        <h2 class="text-center text-accent_wine text-2xl mb-5">{{ __('Inscription') }}</h2>
+        <form method="POST" action="{{ route('register') }}">
+          @csrf
+          <div class="mb-4">
+            <input id="name" type="text" placeholder="{{ __('Nom') }}" class="appearance-none border rounded w-full py-3 px-3 text-accent_wine leading-tight focus:outline-none border-accent_wine @error('name') border-red-500 @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+            @error('name')
+            <p class="text-red-500 text-xs italic mt-2">{{ $message }}</p>
+            @enderror
+          </div>
+          <div class="mb-4">
+            <input id="courriel" type="email" placeholder="{{ __('Courriel') }}" class="appearance-none border rounded w-full py-3 px-3 text-accent_wine leading-tight focus:outline-none border-accent_wine @error('courriel') border-red-500 @enderror" name="courriel" value="{{ old('courriel') }}" required autocomplete="courriel">
+            @error('courriel')
+            <p class="text-red-500 text-xs italic mt-2">{{ $message }}</p>
+            @enderror
+          </div>
+          <div class="mb-4">
+            <input id="password" type="password" placeholder="{{ __('Mot de passe') }}" class="appearance-none border rounded w-full py-3 px-3 text-accent_wine leading-tight focus:outline-none border-accent_wine @error('password') border-red-500 @enderror" name="password" required autocomplete="new-password">
+            @error('password')
+            <p class="text-red-500 text-xs italic mt-2">{{ $message }}</p>
+            @enderror
+          </div>
+          <div class="mb-5">
+            <input id="password-confirm" type="password" placeholder="{{ __('Mod de passe confirmé') }}" class="appearance-none border rounded w-full py-3 px-3 text-accent_wine leading-tight focus:outline-none border-accent_wine" name="password_confirmation" required autocomplete="new-password">
+          </div>
+          <div class="text-center">
+            <button type="submit" class="bg-accent_wine space-x-1 font-normal border text-main hover:border-accent_wine hover:bg-transparent hover:text-accent_wine py-2 px-5 uppercase rounded focus:outline-none">
+              {{ __('Inscription') }}
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,9 +1,9 @@
-<nav class="bg-gray-50 shadow-sm">
+<nav id="main-nav" class="lg:bg-gray-50 lg:shadow-sm md:bg-transparent md:shadow-none py-1">
   <div class="max-w-8xl mx-auto px-2 sm:px-6 lg:px-8">
     <div class="relative flex items-center justify-between h-16">
       <!-- Logo -->
       <div class="absolute inset-y-0 left-0 flex items-center lg:hidden">
-        <button id="mobile-menu-button" type="button" class="fixed top-3 right-3 inline-flex items-center justify-center p-2  rounded-md text-section_title hover:text-accent_wine hover:border-accent_wine border-3  focus:outline-none focus:border-accent_wine focus:text-accent_wine transition duration-150 ease-in-out">
+        <button id="mobile-menu-button" type="button" class="z-10 fixed top-3 right-3 inline-flex items-center justify-center p-2  rounded-md text-section_title hover:text-accent_wine hover:border-accent_wine border-3  focus:outline-none focus:border-accent_wine focus:text-accent_wine transition duration-150 ease-in-out">
           <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
@@ -16,47 +16,46 @@
         </div>
         <div class="hidden sm:block sm:ml-6">
           <ul class="items-center justify-between space-x-6 hidden lg:flex ">
-            <li>
-              <a href="{{ route('celliers.index') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-section_title" aria-label="celliers" title="celliers">
-                Celliers
-              </a>
-            </li>
             @guest
-            <li>
-              <a href="{{ route('home') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-section_title" aria-label="articles" title="articles">
-                Articles
-              </a>
-            </li>
-            <li>
-              <a href="{{ route('bouteilles') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-section_title" aria-label="saq" title="saq">
-                SAQ
-              </a>
-            </li>
+
             <li>
               <a href="{{ route('login') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-section_title" aria-label="sign-in" title="sign-in">
-                Connecter
+                Connexion
               </a>
             </li>
 
             <li>
               <a href="{{ route('register') }}" class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-main transition duration-200 rounded bg-accent_wine hover:bg-transparent hover:border border-accent_wine hover:text-accent_wine" aria-label="sign-up" title="sign-up">
-                S'inscrire
+                Inscription
               </a>
             </li>
             @else
             <li>
+              <a href="{{ route('celliers.index') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-section_title" aria-label="celliers" title="celliers">
+                Mes Celliers
+              </a>
+            </li>
+            <li>
+              <a href="{{ route('home') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-section_title" aria-label="articles" title="articles">
+                Ajouter Cellier
+              </a>
+            </li>
+            <li>
+              <a href="{{ route('bouteilles') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-section_title" aria-label="saq" title="saq">
+                Ajouter Bouteille
+              </a>
+            </li>
+            <li>
               <a href="{{ route('logout') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-gray-500" aria-label="Sign out" title="deconnecter" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                Déconnecter
+                Déconnexion
               </a>
               <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">@csrf</form>
             </li>
             <li>
-              <a href="#" class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-accent_wine transition duration-200 rounded shadow-md border border-accent_wine hover:bg-accent_wine hover:text-main focus:shadow-outline focus:outline-none" aria-label="Compte" title="Compte">
-                Compte
+              <a href="#" class="inline-flex gap-2 items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-secondary transition duration-200 rounded  hover:text-accent_wine focus:shadow-outline focus:outline-none" aria-label="Compte" title="Compte">
+                <img class="max-w-[40px]" src="{{ asset('img/svg/user-gold.svg') }}" alt="user-profile">
+                {{ Auth::user()->nom }} <!--  Affichage du nom Utilisateur. À modifier   -->
               </a>
-            </li>
-            <li>
-              {{ Auth::user()->nom }} <!--  Affichage du nom Utilisateur. À modifier   -->
             </li>
             @endguest
           </ul>
@@ -65,39 +64,39 @@
     </div>
   </div>
   <!-- Mobile Menu -->
-  <div id="mobile-menu" class="hidden lg:hidden">
-    <div class="space-y-4 flex flex-col items-center justify-center">
+  <!--  from-accent_wine to-main bg-gradient-to-t -->
+  <div id="mobile-menu" class="transition duration-300 hidden lg:hidden from-accent_wine to-main bg-gradient-to-t absolute top-0 left-0 w-full">
+    <div class="space-y-7 flex flex-col items-center justify-center mt-20 pb-10">
       <a href="/" class="text-accent_wine uppercase tracking-wide font-bold pb-4"><img src="{{ asset('img/svg/logoWN.svg') }}" alt="logo-wineNot" class="mx-auto"></a>
-      <a href="{{ route('celliers.index') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-section_title" aria-label="celliers" title="celliers">
-        Celliers
-      </a>
       @guest
-      <a href="{{ route('home') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-section_title" aria-label="articles" title="articles">
-        Articles
-      </a>
-      <a href="{{ route('bouteilles') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-section_title" aria-label="saq" title="saq">
-        SAQ
-      </a>
       <a href="{{ route('login') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-section_title" aria-label="sign-in" title="sign-in">
-        Connecter
+        Connexion
       </a>
-      <a href="{{ route('register') }}" class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-main transition duration-200 bg-accent_wine hover:bg-transparent hover:border border-accent_wine hover:text-accent_wine" aria-label="sign-up" title="sign-up">
-        S'inscrire
+      <a href="{{ route('register') }}" class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-main transition duration-200 bg-accent_wine  hover:bg-transparent hover:border border-accent_wine hover:text-accent_wine" aria-label="sign-up" title="sign-up">
+        Inscription
       </a>
 
       @else
-      <a href="{{ route('logout') }}" class="justify-center font-medium tracking-wide text-accent_wine transition-colors duration-200 hover:text-gray-500" aria-label="Sign out" title="deconnecter" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-        Déconnecter
+      <!-- font-medium tracking-wide text-accent_wine -->
+      <a href="{{ route('celliers.index') }}" class="justify-center font-light tracking-wide text-xl text-main transition-colors duration-200 hover:text-section_title" aria-label="celliers" title="celliers">
+        Mes Celliers
       </a>
-      <a>
+      <a href="{{ route('home') }}" class="justify-center font-light tracking-wide text-xl text-main transition-colors duration-200 hover:text-section_title" aria-label="articles" title="articles">
+        Ajouter Cellier
+      </a>
+      <a href="{{ route('bouteilles') }}" class="justify-center font-light tracking-wide text-xl text-main transition-colors duration-200 hover:text-section_title" aria-label="saq" title="saq">
+        Ajouter Bouteille
+      </a>
+      <a href="{{ route('logout') }}" class="justify-center font-light tracking-wide text-xl text-main transition-colors duration-200 hover:text-gray-500" aria-label="Sign out" title="deconnecter" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        Déconnexion
         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">@csrf</form>
       </a>
-      <a href="#" class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-accent_wine transition duration-200 border border-accent_wine hover:bg-accent_wine hover:text-main" aria-label="Compte" title="Compte">
-        Compte
-      </a>
-      <a role="nave-link" class="text-article_title">
+      <a href="#" class="pt-2 flex flex-col items-center justify-center text-xl rounded h-12 px-6 font-light tracking-wide text-secondary transition duration-200 hover:text-main" aria-label="Compte" title="Compte"><img class="max-w-[45px]" src="{{ asset('img/svg/user-gold.svg') }}" alt="user-profile">
         {{ Auth::user()->nom }} <!--  Affichage du nom Utilisateur. À modifier   -->
       </a>
+      <!-- <a role="nave-link" class="text-article_title">
+        {{ Auth::user()->nom }}  Affichage du nom Utilisateur. À modifier 
+      </a> -->
       @endguest
     </div>
   </div>
