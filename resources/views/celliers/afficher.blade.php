ff9ce096a2089ed00a1e267c211f9a175b5a2019
Lbarreiros1980 <88776130+Lbarreiros1980@users.noreply.github.com>
2023-04-21 02:39:01 -0400

    ajout du formulaire stylisee pour modifier un cellier.

--- resources/views/celliers/afficher.blade.php
+++ resources/views/celliers/afficher.blade.php
@@ -4,7 +4,7 @@
 <div class="container mx-auto py-8 px-8 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
        <!-- Redirection vers modifier les infos du cellier -->
        <div>
-       <div class="text-center w-full" id="0">
+       <div class="text-center w-full">
         <a  href="{{route('celliers.modifier', ['cellier' => $cellier->id])}}" class="inline-flex items-center justify-center space-x-2 py-3 px-4 bg-accent_wine hover:accent_wine-80 text-main font-bold ml-2 py-2 px-4 rounded focus:outline-none focus:shadow-outline">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -18,7 +18,7 @@
           Modifier les informations du cellier
         </a> --}}
       </div>
-      <div class="py-4 mt-6" id="1">
+  <div class="py-4 mt-6">
     <div onclick="location.href=`{{route('celliers.afficher', $cellier->id)}}`">
       <!--<img class="object-cover w-full h-56 mb-6 rounded shadow-lg md:h-64 xl:h-80" src="/{{$cellier->image}}" alt="{{$cellier->nom}}" />-->
         <h3 class="mb-2 text-3xl font-bold leading-none sm:text-2xl">
@@ -29,8 +29,6 @@
         </p>
     </div>
   </div>
-    </div>
-  
 {{-- ------- formulaire d'ajout de bouteilles ------------ --}}
   <div class="flex flex-col lg:flex-row h-screen items-center">
     <div class="bg-gray-900">
