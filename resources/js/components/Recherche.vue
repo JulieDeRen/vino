32a461108ae20e57cc0073924d2f1c9bea13889e
Raritetnik <mkodarenko@gmail.com>
2023-04-21 11:13:54 -0400

    FIX: affiche Recherche.vue

--- resources/js/components/Recherche.vue
+++ resources/js/components/Recherche.vue
@@ -16,7 +16,7 @@
     </div>
     <div>
       <div class="card flex" v-if="selectedVine" style="max-width: 300px;">
-        <header class="card-header">
+        <header class="card-header" style="max-width: 300px;">
           <img :src="this.choixBouteille.url_img" :alt="this.choixBouteille.nom" class="max-w-none" height="150px">
         </header>
         <div class="card-body">
