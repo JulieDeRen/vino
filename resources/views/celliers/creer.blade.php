e8b2dc111ea813fab0bc0c9d1d1baddc33f14dbf
JulieDeRen <julie.deschenes-renaud@hec.ca>
2023-04-22 13:02:41 -0400

    Modification du formulaire de création de cellier pour reprendre le style général

--- resources/views/celliers/creer.blade.php
+++ resources/views/celliers/creer.blade.php
@@ -1,96 +1,110 @@
 @extends('layouts.app')
 @section('content')
-<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-<form action="{{route('celliers.insererCellier')}}" method="post" enctype="multipart/form-data" class="w-full">
-  @csrf
-  <div>
-    <div class="w-full px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="nom">
-        Nom
-      </label>
-      <input id="nom" name="nom" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="Entrez le nom du cellier" required>
-    </div>
-    <div class="w-full px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="quantite_max">
-        Quantité
-      </label>
-      <input id="quantite_max" name="quantite_max" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="number" placeholder="Capacité maximale du cellier">
-    </div>
-    <div class="w-full px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="description">
-        Description
-      </label>
-      <textarea name="description" id="description" cols="30" rows="10" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Ajouter un descriptif pour ce cellier"></textarea>
-    </div>
-    <div class="w-full px-3 cellier-img">
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellierCaveVinMontWashington.webp">
-        <img src="{{url('img/celliers/cellierCaveVinMontWashington.webp')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/large_Charlevoix-Wine-Cellar-with-Dog.jpg"  checked>
-        <img src="{{url('img/celliers/large_Charlevoix-Wine-Cellar-with-Dog.jpg')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_verre_moderne.jpg">
-        <img src="{{url('img/celliers/cellier_verre_moderne.jpg')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_tonneau.webp">
-        <img src="{{url('img/celliers/cellier_tonneau.webp')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_porte_bois_murs.jpg">
-        <img src="{{url('img/celliers/cellier_porte_bois_murs.jpg')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_mont_killian_bois_pierre.jpg">
-        <img src="{{url('img/celliers/cellier_mont_killian_bois_pierre.jpg')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_moderne_noir.webp">
-        <img src="{{url('img/celliers/cellier_moderne_noir.webp')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_moderne_led.jpg">
-        <img src="{{url('img/celliers/cellier_moderne_led.jpg')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_moderne_decoratif.jpeg">
-        <img src="{{url('img/celliers/cellier_moderne_decoratif.jpeg')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_cuisine.jpeg">
-        <img src="{{url('img/celliers/cellier_cuisine.jpeg')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_contemporain.jpg">
-        <img src="{{url('img/celliers/cellier_contemporain.jpg')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_brique_bois.jpg">
-        <img src="{{url('img/celliers/cellier_brique_bois.jpg')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_bois_verre.jpg">
-        <img src="{{url('img/celliers/cellier_bois_verre.jpg')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_bois_moderne_trad.webp">
-        <img src="{{url('img/celliers/cellier_bois_moderne_trad.webp')}}">
-      </label>
-      <label>
-        <input type="radio" name="image" value="img/celliers/cellier_bois_chateau.jpeg">
-        <img src="{{url('img/celliers/cellier_bois_chateau.jpeg')}}">
-      </label>
-    </div>
+<div class="bg-white py-16">
+  <div class="container mx-auto py-5 px-8 max-w-lg">
+    <h2 class="text-4xl font-bold mb-8">Ajouter Un Cellier</h2>
+    <p class="text-gray-700 mb-4">Veuillez remplir le formulaire suivant pour ajouter un cellier</p>
+    <form action="{{route('celliers.insererCellier')}}" method="post" enctype="multipart/form-data" class="w-full">
+      <!-- ajouter un token pour autoriser la route une seconde fois -->
+      <!-- méthode put pour modification -->
+      @csrf
+      @method('PUT')
+      <div class="mb-4">
+        <label class="block text-gray-700 font-bold mb-2" for="nom">
+          Nom
+        </label>
+        <!-- ** important de concerver les input ** -->
+        <input class="w-full items-center justify-center h-12 px-6 font-medium tracking-wide text-accent_wine transition duration-200 rounded border border-accent_wine focus:shadow-outline" id="nom" name="nom" type="text" placeholder="Entrez le nom du cellier" required>
+      </div>
+      <div class="mb-4">
+        <label class="block text-gray-700 font-bold mb-2" for="quantite_max">
+          Quantité
+        </label>
+        <input class="w-full items-center justify-center h-12 px-6 font-medium tracking-wide text-accent_wine transition duration-200 rounded border border-accent_wine focus:shadow-outline" id="quantite_max" name="quantite_max" type="number" placeholder="Capacité maximale du cellier">
+      </div>
+      <div class="mb-4">
+        <label class="block text-gray-700 font-bold mb-2" for="description">
+          Description
+        </label>
+        <textarea class="w-full h-full items-center justify-center h-12 px-6 font-medium tracking-wide text-accent_wine transition duration-200 rounded border border-accent_wine focus:shadow-outline" id="description" name="description" type="text" placeholder="Ajouter un descriptif pour ce cellier"></textarea>
+      </div>
+      <div class="cellier-img">
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellierCaveVinMontWashington.webp">
+          <img src="{{url('img/celliers/cellierCaveVinMontWashington.webp')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/large_Charlevoix-Wine-Cellar-with-Dog.jpg" checked>
+          <img src="{{url('img/celliers/large_Charlevoix-Wine-Cellar-with-Dog.jpg')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_verre_moderne.jpg">
+          <img src="{{url('img/celliers/cellier_verre_moderne.jpg')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_tonneau.webp">
+          <img src="{{url('img/celliers/cellier_tonneau.webp')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_porte_bois_murs.jpg">
+          <img src="{{url('img/celliers/cellier_porte_bois_murs.jpg')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_mont_killian_bois_pierre.jpg">
+          <img src="{{url('img/celliers/cellier_mont_killian_bois_pierre.jpg')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_moderne_noir.webp">
+          <img src="{{url('img/celliers/cellier_moderne_noir.webp')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_moderne_led.jpg">
+          <img src="{{url('img/celliers/cellier_moderne_led.jpg')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_moderne_decoratif.jpeg">
+          <img src="{{url('img/celliers/cellier_moderne_decoratif.jpeg')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_cuisine.jpeg">
+          <img src="{{url('img/celliers/cellier_cuisine.jpeg')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_contemporain.jpg">
+          <img src="{{url('img/celliers/cellier_contemporain.jpg')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_brique_bois.jpg">
+          <img src="{{url('img/celliers/cellier_brique_bois.jpg')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_bois_verre.jpg">
+          <img src="{{url('img/celliers/cellier_bois_verre.jpg')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_bois_moderne_trad.webp">
+          <img src="{{url('img/celliers/cellier_bois_moderne_trad.webp')}}">
+        </label>
+        <label>
+          <input type="radio" name="image" value="img/celliers/cellier_bois_chateau.jpeg">
+          <img src="{{url('img/celliers/cellier_bois_chateau.jpeg')}}">
+        </label>
+      </div>
+      <div class="mb-4 py-4 text-center">
+        <input value = "Modifier" class="bg-accent_wine hover:accent_wine-80 text-main font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline" type="submit" placeholder="Créer le cellier">
+        </input>
+      </div>
+    </form>
+  </div>
+</div>
+</div>
 
-    <div class="w-full px-3">
+<!-- <div class="w-full px-3">
       <input class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-accent_wine transition duration-200 rounded shadow-md border border-accent_wine hover:bg-accent_wine hover:text-main focus:shadow-outline focus:outline-none" 
       type="submit" 
       placeholder="Créer le cellier">
-    </div>
-  </div>
+    </div> -->
+</div>
 </form>
 </div>
 @endsection
